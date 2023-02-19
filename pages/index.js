b89b131e4aa430e3ae9b636b82768f160aa22aab
hwn0809 <hwn0809@gmail.com>
2023-02-19 20:11:35 +0900

    fix header not fully expanding

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main>
-      <div className="container">
-        <div className="wrapper">
+      <div className="">
+        <div className="w-full h-full">
           <h5>
             Lorem <b>ipsum</b> dolor sit amet, consectetur adipisicing elit.
             Cumque adipisci rerum vel in rem sapiente ut eos delectus quo
